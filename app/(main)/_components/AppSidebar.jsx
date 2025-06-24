063e53df638c3618e493299cff3dcf235af91f67
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2025-06-24 14:42:13 +0530

    changed name

--- app/(main)/_components/AppSidebar.jsx
+++ app/(main)/_components/AppSidebar.jsx
@@ -53,7 +53,7 @@ function AppSidebar() {
         <div className='flex items-center gap-3 w-full justify-center mt-5'>
         
             <Image src={'/logo.svg'} alt='logo' width={40} height={40}/>
-            <h2 className='font-bold text-2xl'>GenVid</h2>
+            <h2 className='font-bold text-2xl'>Kwik Reels</h2>
         
         </div>
         </Link>
